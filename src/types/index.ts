a77e17330c3a73dc62763ef11748848e4c06fdce
Dylan <dydy86100@gmail.com>
2024-07-02 23:06:39 +0200

    Update index.ts

--- src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './Type';
-export * from './Move';
-export * from './Stat';
-export * from './Ability';
-export * from './Sprite';
-export * from './Pokemon';
-export * from './MinimalPokemon';
