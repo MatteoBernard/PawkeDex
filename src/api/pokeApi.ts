c0cb70bd88bb616e1899b4e0f30fe94d75223754
Dylan <dydy86100@gmail.com>
2024-07-02 23:07:18 +0200

    Create pokeApi.ts

---
+++ src/api/pokeApi.ts
@@ -0,0 +1,37 @@
+import {LocationClient, Pokemon, PokemonClient} from "pokenode-ts";
+
+const pokemonClient: PokemonClient = new PokemonClient();
+const locationClient: LocationClient = new LocationClient();
+
+export const fetchPokemons = async (): Promise<{name:string, url:string}[]> => {
+    return await pokemonClient.listPokemons(0, 1302)
+        .then((response) => {
+            console.log(response.results);
+            return response.results;
+        })
+        .catch((error) => {
+            throw error;
+        });
+}
+
+export const fetchAllRegions = async (): Promise<{name:string, url:string}[]> => {
+    return await locationClient.listRegions()
+        .then((response) => {
+            console.log(response.results);
+            return response.results;
+        })
+        .catch((error) => {
+            throw error;
+        });
+}
+
+export const fetchPokemon = async (id: number): Promise<Pokemon> => {
+    return await pokemonClient.getPokemonById(id)
+        .then((response) => {
+            console.log(response);
+            return response;
+        })
+        .catch((error) => {
+            throw error;
+        });
+}
