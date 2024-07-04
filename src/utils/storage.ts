880a357a4de66583e5862b37b8ba895439ab1aa8
Bernard Mattéo <matteo_bernard1@ens.univ-artois.fr>
2024-07-04 21:58:21 +0200

    Fix storage util

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -8,22 +8,25 @@ export const storeFavoritePokemon = async (name: string, url: string) => {
     }
 }
 
-export const getFavoritePokemon = async (name: string) => {
+export const getFavoritePokemon = async (name: string): Promise<{name: string, url: string}> => {
     try {
-        const url = await AsyncStorage.getItem(name);
+        let url = await AsyncStorage.getItem(name);
+        url = url ? url : '';
         return {name, url};
     } catch (e) {
         console.log(e);
+        return {name: '', url: ''};
     }
 }
 
-export const getAllFavoritePokemon = async () => {
+export const getAllFavoritePokemon = async (): Promise<{name: string, url: string}[]> => {
     try {
-        const names = await AsyncStorage.getAllKeys();
-        const urls = await AsyncStorage.multiGet(names);
-        return names.map(([name, url]) => ({name, url}));
+        const keys = await AsyncStorage.getAllKeys();
+        const result = await AsyncStorage.multiGet(keys);
+        return result.map(([name, url]) => ({name, url: url ? url : ''}));
     } catch (e) {
         console.log(e);
+        return [];
     }
 }
 
